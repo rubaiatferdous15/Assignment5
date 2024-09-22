b2e5e6d9ef8a7ab98be6da8008eb8d8332e0fcdd
Rubaiat15 <rubaiatferdous15@gmail.com>
2024-09-23 03:17:42 +0600

    ai sala

--- js/common.js
+++ js/common.js
@@ -3,3 +3,25 @@ function getInputValue(id){
    const inputNumber = parseFloat(inputValue);
    return inputNumber;
 }
+
+
+function getTotal(id1,a){
+    const total = parseFloat(document.getElementById(id1).innerText)+parseFloat(a);
+    
+     return document.getElementById(id1).innerText = total;
+
+
+     
+    
+}
+
+
+function totalBalance(id,b){
+    const overall = parseFloat (document.getElementById(id).innerText);
+
+    const leftBalance = parseFloat(overall) - parseFloat(b);
+
+    return  document.getElementById('total-balance').innerText = parseFloat(leftBalance);
+
+
+}
--- js/functions.js
+++ js/functions.js
@@ -4,15 +4,22 @@ document.getElementById('noakhali-btn').addEventListener('click',function(event)
     console.log(addMoney);
 
 
+    if(isNaN(addMoney)){
+        alert("NOOOOO");
+        return;
+    }
+
+
     
 
-    const total1 =parseFloat(document.getElementById('noakhali-balance').innerText)+parseFloat(addMoney);
-    console.log(total1);
+    
 
 
-    document.getElementById('noakhali-balance').innerText = total1;
+    const total1 = getTotal('noakhali-balance',addMoney);
 
+   
 
+    const left  = totalBalance('total-balance',total1);
 
 
 })
@@ -25,14 +32,15 @@ document.getElementById('feni-btn').addEventListener('click',function(event){
     const addMoney1 = getInputValue('feni-donation');
     console.log(addMoney1);
 
-
+    if(isNaN(addMoney1)){
+        alert("NOOOOO");
+        return;
+    }
     
 
-    const total2 =parseFloat(document.getElementById('feni-balance').innerText)+parseFloat(addMoney1);
-    console.log(total2);
+     const total2 = getTotal('feni-balance',addMoney1);
 
-
-    document.getElementById('feni-balance').innerText = total2;
+     const left  = totalBalance('total-balance',total2);
 
 
 
@@ -42,17 +50,17 @@ document.getElementById('feni-btn').addEventListener('click',function(event){
 
 document.getElementById('quota-btn').addEventListener('click',function(event){
     event.preventDefault();
-    const addMoney = getInputValue('quota-donation');
-    console.log(addMoney);
+    const addMoney2 = getInputValue('quota-donation');
+    console.log(addMoney2);
 
+    if(isNaN(addMoney2)){
+        alert("NOOOOO");
+        return;
+    }
 
     
-
-    const total1 =parseFloat(document.getElementById('quota-balance').innerText)+parseFloat(addMoney);
-    console.log(total1);
-
-
-    document.getElementById('quota-balance').innerText = total1;
+    const total3 = getTotal('quota-balance',addMoney2);
+    const left  = totalBalance('total-balance',total3);
 
 
 
